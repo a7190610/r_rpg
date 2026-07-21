dd507e402bb5b5b758774e0980c28dfdc49f501b
a7190610 <107949125+a7190610@users.noreply.github.com>
2026-07-21 13:14:16 +0800

    Add music URLs to RO_DATABASE_MUSIC

--- ro_database_music.js
+++ ro_database_music.js
@@ -1,3 +1,5 @@
+//https://nextly.blogspot.com/2009/07/onlineragnarok-online-bgm.html
+//https://downloads.khinsider.com/game-soundtracks/album/ragnarok-online-original-soundtrack
 export const RO_DATABASE_MUSIC = {
     MUSIC: {
         'prt_fild08': 'https://downloads.khinsider.com/game-soundtracks/album/ragnarok-online-original-soundtrack/1-12.%2520Streamside.mp3', // 普隆德拉原野
