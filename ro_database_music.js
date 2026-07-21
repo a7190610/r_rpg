800722a5f891ec6c0fc5c1a662aec19a1d6c27dd
a7190610 <107949125+a7190610@users.noreply.github.com>
2026-07-21 13:27:52 +0800

    Update ro_database_music.js

--- ro_database_music.js
+++ ro_database_music.js
@@ -2,7 +2,7 @@
 //https://downloads.khinsider.com/game-soundtracks/album/ragnarok-online-original-soundtrack
 export const RO_DATABASE_MUSIC = {
     MUSIC: {
-        'prt_fild08': 'https://downloads.khinsider.com/game-soundtracks/album/ragnarok-online-original-soundtrack/1-12.%2520Streamside.mp3', // 普隆德拉原野
+        'prt_fild08': 'https://nu.vgmtreasurechest.com/soundtracks/ragnarok-online-original-soundtrack/ntcesypd/1-12.%20Streamside.mp3', // 普隆德拉原野
         'pay_fild04': 'https://你存放音樂的網址/pay_fild04.mp3', // 斐揚樹林
         // 依此類推對應各個地圖的 ID
     }
